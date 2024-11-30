7f17ad7d94929a2e9c4cabe72b7975344bd93a2e
mjk02123 <k64837611@gmail.com>
2024-11-30 22:40:16 +0900

    feat | 섬 화면 Tutorial
    처음Tutorial 화면 수정 필요

--- Assets/WorkSpace/KMJ Scripts/TutorialPopupOff.cs
+++ Assets/WorkSpace/KMJ Scripts/TutorialPopupOff.cs
@@ -3,8 +3,23 @@ using UnityEngine.EventSystems;
 
 public class TutorialPopupOff : MonoBehaviour, IPointerClickHandler
 {
+    public GameObject nextPanel; 
+
+    void Start()
+    {
+        if (nextPanel != null)
+        {
+            nextPanel.SetActive(false);
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         gameObject.SetActive(false);
+
+        if (nextPanel != null)
+        {
+            nextPanel.SetActive(true);
+        }
     }
 }
